610cdffdb28ec163ea4d790d56bab8fdb6802460
Blake <btkruppa513@gmail.com>
2019-11-07 11:00:33 -0500

    added more angular demos

--- angular/angular-demos/src/app/components/clicker/clicker.component.ts
+++ angular/angular-demos/src/app/components/clicker/clicker.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clicker',
   templateUrl: './clicker.component.html',
   styleUrls: ['./clicker.component.scss']
 })
-export class ClickerComponent implements OnInit {
+export class ClickerComponent implements OnInit, OnDestroy {
+
+  clickAmount = 0;
+  autoAmount = 0;
+
+  autoInterval: NodeJS.Timer;
+
+  plusTwoUnlocked = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.autoInterval = setInterval(() => {
+      this.increment(this.autoAmount);
+    }, 1000);
   }
 
+  increment(amount: number): void {
+    this.clickAmount += amount;
+  }
+
+  incrementAuto(amount: number): void {
+    this.autoAmount += amount;
+    this.clickAmount -= 25 * amount;
+  }
+
+  unlockPlusTwo() {
+    this.plusTwoUnlocked = true;
+    this.clickAmount -= 100;
+  }
+
+  ngOnDestroy() {
+    console.log('removing interval');
+    clearInterval(this.autoInterval);
+  }
 }
